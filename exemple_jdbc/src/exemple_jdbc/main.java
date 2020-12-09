0e50cbee058592e17f4e3462d498f1a1adc3dec2
jmllubes.github.io <jmllubes@gmail.com>
2020-12-09 10:28:00 +0100

    select

--- exemple_jdbc/src/exemple_jdbc/main.java
+++ exemple_jdbc/src/exemple_jdbc/main.java
@@ -19,7 +19,7 @@ public class main {
 	    Statement stmt = conn.createStatement( );
 	    int row = stmt.executeUpdate(sql);	    
 	    System.out.println("Files afectades " + row );*/
-	    String sql = "INSERT INTO dept VALUES(?,?,?) ";
+	    /*String sql = "INSERT INTO dept VALUES(?,?,?) ";
 	    int codi = 13;
 	    String nom = "administratiu";
 	    String local = "Mollerussa";
@@ -27,7 +27,20 @@ public class main {
 	    pstmt.setInt(1, codi);
 	    pstmt.setString(2, nom);
 	    pstmt.setString(3, local);
-	    pstmt.executeUpdate();
+	    pstmt.executeUpdate();*/
+	    
+	    String sql = "SELECT c.COM_NUM,cl.NOM,c.client_cod,c.total,cl.adreca FROM comanda as c INNER JOIN client as cl"
+	    	+ " ON c.client_cod = cl.client_cod";
+	    Statement stmt = conn.createStatement();
+	    ResultSet result = stmt.executeQuery(sql);
+	    
+	    while(result.next()) {
+		System.out.print("COM_NUM: " + result.getInt(1) + "  ");
+		System.out.print("CLIENT_COD: " + result.getInt(3) + "  ");
+		System.out.print("NOM: " + result.getString(2) + "  ");
+		System.out.print("ADREÇA: " + result.getString(5) + "  ");
+		System.out.println("TOTAL: " + result.getFloat(4));
+	    }
 	    
 	    conn.close();
 	    
